cb0f36aebc12499bd24fc947d7546b4815da1011
dragonfly95 <dQzNKCyAgf43e3B>
2021-04-22 22:42:03 +0900

    sdf

--- pgDBConn.js
+++ pgDBConn.js
@@ -1,16 +1,15 @@
-const vals = require('./const.js');
 const { Pool, Client } = require('pg');
 const client = new Client({
-    user: vals.user,
-    password: vals.pass,
-    host: vals.host,
-    port: vals.port,
-    database: vals.db
+    user: 'postgres',
+    password: '',
+    host: 'localhost',
+    port: '5432',
+    database: 'ngrid'
 });
 
 function GetUserList() {
     client.connect();
-    client.query('SELECT * FROM users', (err, res) => {
+    client.query('SELECT * FROM stat.content_info', (err, res) => {
         console.log(res);
         client.end();
     });
