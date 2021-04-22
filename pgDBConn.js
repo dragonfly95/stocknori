958a1a19fb53f0d4ea29ae6075abb4ea1a694bea
dragonfly95 <dQzNKCyAgf43e3B>
2021-04-22 22:58:31 +0900

    sdf

--- pgDBConn.js
+++ pgDBConn.js
@@ -1,7 +1,7 @@
 const { Pool, Client } = require('pg');
 const client = new Client({
     user: 'postgres',
-    password: '',
+    password: '123456',
     host: 'localhost',
     port: '5432',
     database: 'ngrid'
