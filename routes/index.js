58eeea7255b6973183c6b1ed15de273b2d35b527
dragonfly95 <dQzNKCyAgf43e3B>
2021-04-24 09:46:56 +0900

    v

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/s', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login.do', (req, res) => {
+  res.render('login', { title: 'Login' })
+})
+
+
+router.get('/data1.do', (req, res) => {
+  res.render('data1', { title: 'data1' })
+})
 
 module.exports = router;
